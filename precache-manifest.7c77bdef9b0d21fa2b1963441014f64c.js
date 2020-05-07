da6f602f83c28e092fa33c38d7fb321561b46212
IgorB <iblanusa@gmail.com>
2020-05-07 12:57:38 +0200

    Updates

---
+++ precache-manifest.7c77bdef9b0d21fa2b1963441014f64c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad67bcb13f055d812b86a8c8b7ab488f",
+    "url": "/ToDo/index.html"
+  },
+  {
+    "revision": "490f73032798af6325e4",
+    "url": "/ToDo/static/css/main.74c5bf8e.chunk.css"
+  },
+  {
+    "revision": "44b9eb92d603bd407488",
+    "url": "/ToDo/static/js/2.074f12b4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ToDo/static/js/2.074f12b4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "490f73032798af6325e4",
+    "url": "/ToDo/static/js/main.38ec9728.chunk.js"
+  },
+  {
+    "revision": "1fd6913d42a7e0443cc0",
+    "url": "/ToDo/static/js/runtime-main.53e319bd.js"
+  }
+]);
